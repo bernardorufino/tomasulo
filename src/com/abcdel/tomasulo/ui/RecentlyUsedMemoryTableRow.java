966a966b383c75511fc57f66b44ff83ec47ff3cf
Victor <vicrmoura@gmail.com>
2014-06-26 13:36:56 -0300

    Some improvements in table ui

---
+++ src/com/abcdel/tomasulo/ui/RecentlyUsedMemoryTableRow.java
@@ -0,0 +1,26 @@
+package com.abcdel.tomasulo.ui;
+
+public class RecentlyUsedMemoryTableRow {
+
+  private String mID;
+  private String mType;
+  private String mBusy;
+  private String mState;
+  private String mVj;
+  private String mVk;
+  private String mQj;
+  private String mQk;
+  private String mA;
+
+  public RecentlyUsedMemoryTableRow(String id, String type, String busy, String state, String vj, String vk, String qj, String qk, String a) {
+    mA = a;
+    mType = type;
+    mBusy = busy;
+    mID = id;
+    mState = state;
+    mVj = vj;
+    mVk = vk;
+    mQj = qj;
+    mQk = qk;
+  }
+}
