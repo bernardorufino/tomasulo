53d9b6450c7f0c039e3654fcdaf7a23fee5a17ae
Victor <vicrmoura@gmail.com>
2014-06-26 18:34:10 -0300

    Button logic, ui state and mock simulator

--- src/com/abcdel/tomasulo/ui/MainApplication.java
+++ src/com/abcdel/tomasulo/ui/MainApplication.java
@@ -4,6 +4,11 @@ import com.abcdel.tomasulo.simulator.ReserveStation;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import com.abcdel.tomasulo.simulator.MockSimulator;
+import com.abcdel.tomasulo.simulator.Simulator;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -19,21 +24,33 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Field;
 import java.util.Observable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApplication extends Application {
 
     private static final int BUTTON_IMAGE_DIMENSION = 15;
+
+    private Stage mPrimaryStage;
+    private Button mPlayButton;
+    private Button mPauseButton;
+    private Button mStopButton;
+    private Button mStepButton;
+    private Button mFileButton;
+
+    private final List<ApplicationListener> mListeners = new ArrayList<ApplicationListener>();
+    private ApplicationState mApplicationState;
+    private Simulator mSimulator;
 
     public static void main(String[] args) {
         launch(args);
@@ -41,8 +58,12 @@ public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) {
+        mPrimaryStage = stage;
+
+        mSimulator = new MockSimulator(this);
+
         Scene scene = new Scene(new Group(), 900, 600);
-        stage.setTitle("Main Application");
+
         ScrollPane sp = new ScrollPane();
         sp.setStyle("-fx-background: rgb(80,80,80);");
         sp.prefHeightProperty().bind(scene.heightProperty());
@@ -50,10 +71,74 @@ public class MainApplication extends Application {
         sp.setFitToWidth(true);
         sp.setFitToHeight(true);
         sp.setContent(createContentPane());
+
         ((Group) scene.getRoot()).getChildren().addAll(sp);
-        //stage.setMaximized(true);
         stage.setScene(scene);
+
+        stage.setTitle("Main Application");
+        stage.setMaximized(true);
         stage.show();
+    }
+
+    public ApplicationState getApplicationState() {
+        return mApplicationState;
+    }
+
+    public void setApplicationState(ApplicationState applicationState) {
+        mApplicationState = applicationState;
+        switch (applicationState) {
+            case STAND_BY:
+                mPlayButton.setVisible(true);
+                mPauseButton.setVisible(false);
+
+                mPlayButton.setDisable(true);
+                mStepButton.setDisable(true);
+                mStopButton.setDisable(true);
+                mFileButton.setDisable(false);
+                clearData();
+                break;
+            case LOADED:
+                mPlayButton.setVisible(true);
+                mPauseButton.setVisible(false);
+
+                mPlayButton.setDisable(false);
+                mStepButton.setDisable(false);
+                mStopButton.setDisable(true);
+                mFileButton.setDisable(false);
+                break;
+            case RUNNING:
+                mPlayButton.setVisible(false);
+                mPauseButton.setVisible(true);
+
+                mStepButton.setDisable(true);
+                mStopButton.setDisable(false);
+                mFileButton.setDisable(true);
+                break;
+            case PAUSED:
+                mPlayButton.setVisible(true);
+                mPauseButton.setVisible(false);
+
+                mPlayButton.setDisable(false);
+                mStepButton.setDisable(false);
+                mStopButton.setDisable(false);
+                mFileButton.setDisable(true);
+                break;
+            case STEPPING:
+                mPlayButton.setVisible(true);
+                mPauseButton.setVisible(false);
+
+                mPlayButton.setDisable(true);
+                mStepButton.setDisable(true);
+                mStopButton.setDisable(false);
+                mFileButton.setDisable(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void addListener(ApplicationListener listener) {
+        mListeners.add(listener);
     }
 
     private Node createContentPane() {
@@ -62,15 +147,17 @@ public class MainApplication extends Application {
         ToolBar toolBar = new ToolBar();
         borderPane.setTop(toolBar);
 
-        Region spacer = new Region();
-        spacer.getStyleClass().setAll("spacer");
+        Region spacerLeft = new Region();
+        spacerLeft.getStyleClass().setAll("spacer");
+        Region spacerCenter = new Region();
+        spacerCenter.getStyleClass().setAll("spacer");
 
         HBox buttonBar = new HBox();
         buttonBar.getStyleClass().setAll("segmented-button-bar");
 
-        Button playButton = new Button();
+        mPlayButton = new Button();
         try {
-            playButton.setGraphic(new ImageView(new Image(
+            mPlayButton.setGraphic(new ImageView(new Image(
                     new FileInputStream("res/icons/play.png"),
                     BUTTON_IMAGE_DIMENSION,
                     BUTTON_IMAGE_DIMENSION,
@@ -79,11 +166,23 @@ public class MainApplication extends Application {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        playButton.getStyleClass().addAll("first");
+        mPlayButton.getStyleClass().addAll("first");
 
-        Button stopButton = new Button();
+        mPauseButton = new Button();
         try {
-            stopButton.setGraphic(new ImageView(new Image(
+            mPauseButton.setGraphic(new ImageView(new Image(
+                    new FileInputStream("res/icons/pause.png"),
+                    BUTTON_IMAGE_DIMENSION,
+                    BUTTON_IMAGE_DIMENSION,
+                    true,
+                    true)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        mStopButton = new Button();
+        try {
+            mStopButton.setGraphic(new ImageView(new Image(
                     new FileInputStream("res/icons/stop.png"),
                     BUTTON_IMAGE_DIMENSION,
                     BUTTON_IMAGE_DIMENSION,
@@ -93,9 +192,9 @@ public class MainApplication extends Application {
             e.printStackTrace();
         }
 
-        Button stepButton = new Button();
+        mStepButton = new Button();
         try {
-            stepButton.setGraphic(new ImageView(new Image(
+            mStepButton.setGraphic(new ImageView(new Image(
                     new FileInputStream("res/icons/step.png"),
                     BUTTON_IMAGE_DIMENSION,
                     BUTTON_IMAGE_DIMENSION,
@@ -105,9 +204,9 @@ public class MainApplication extends Application {
             e.printStackTrace();
         }
 
-        Button openButton = new Button();
+        mFileButton = new Button();
         try {
-            openButton.setGraphic(new ImageView(new Image(
+            mFileButton.setGraphic(new ImageView(new Image(
                     new FileInputStream("res/icons/folder.png"),
                     BUTTON_IMAGE_DIMENSION,
                     BUTTON_IMAGE_DIMENSION,
@@ -116,11 +215,14 @@ public class MainApplication extends Application {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        openButton.getStyleClass().addAll("last", "capsule");
+        mFileButton.getStyleClass().addAll("last", "capsule");
 
-        buttonBar.getChildren().addAll(playButton, stopButton, stepButton, openButton);
-        toolBar.getItems().addAll(spacer, buttonBar);
+        buttonBar.getChildren().addAll(mPlayButton, mPauseButton, mStopButton, mStepButton, mFileButton);
 
+        final Label loadedFileLabel = new Label("No File was loaded");
+        loadedFileLabel.setTextFill(Color.BLACK);
+
+        toolBar.getItems().addAll(spacerLeft, buttonBar, spacerCenter, loadedFileLabel);
         VBox vbox = new VBox();
         vbox.setPadding(new Insets(10, 10, 10, 10));
         vbox.setSpacing(10);
@@ -129,6 +231,55 @@ public class MainApplication extends Application {
         vbox.getChildren().add(createSecondaryTables());
 
         borderPane.setCenter(vbox);
+
+        mPlayButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                for (ApplicationListener listener : mListeners) {
+                    listener.onPlay();
+                }
+            }
+        });
+        mPauseButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                for (ApplicationListener listener : mListeners) {
+                    listener.onPause();
+                }
+            }
+        });
+        mStopButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                for (ApplicationListener listener : mListeners) {
+                    listener.onStop();
+                }
+            }
+        });
+        mStepButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                for (ApplicationListener listener : mListeners) {
+                    listener.onStep();
+                }
+            }
+        });
+        mFileButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                File file = fileChooser.showOpenDialog(mPrimaryStage);
+
+                if (file == null) return;
+                loadedFileLabel.setText("Loaded File: " + file.getPath());
+                for (ApplicationListener listener : mListeners) {
+                    listener.onFileLoaded(file);
+                }
+            }
+        });
+
+        setApplicationState(ApplicationState.STAND_BY);
+
         return borderPane;
     }
 
@@ -184,6 +335,22 @@ public class MainApplication extends Application {
         vbox.setSpacing(5);
         vbox.getChildren().addAll(label, table);
         return vbox;
+    }
+
+    private void clearData() {
+
+    }
+
+    public enum ApplicationState {
+        STAND_BY, LOADED, RUNNING, PAUSED, STEPPING
+    }
+
+    public interface ApplicationListener {
+        public void onFileLoaded(File file);
+        public void onPlay();
+        public void onStop();
+        public void onStep();
+        public void onPause();
     }
 }
 
