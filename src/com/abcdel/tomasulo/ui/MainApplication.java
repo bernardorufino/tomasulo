8cb5b06a0150161c8d3721dfc142d21f59693f85
Victor <vicrmoura@gmail.com>
2014-06-26 13:36:56 -0300

    Some improvements in table ui

--- src/com/abcdel/tomasulo/ui/MainApplication.java
+++ src/com/abcdel/tomasulo/ui/MainApplication.java
@@ -5,20 +5,14 @@ import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainApplication extends Application {
 
@@ -31,6 +25,7 @@ public class MainApplication extends Application {
         Scene scene = new Scene(new Group(), 900, 600);
         stage.setTitle("Main Application");
         ScrollPane sp = new ScrollPane();
+        sp.setStyle("-fx-background: rgb(80,80,80);");
         sp.prefHeightProperty().bind(scene.heightProperty());
         sp.prefWidthProperty().bind(scene.widthProperty());
         sp.setContent(createContentPane());
@@ -50,23 +45,6 @@ public class MainApplication extends Application {
         return vbox;
     }
 
-    private VBox createReserveStationTable() {
-        final Label label = new Label("Reserve Stations");
-        label.setFont(new Font("Arial", 20));
-        final VBox vbox = new VBox();
-        TableView table = new TableView();
-        table.setEditable(false);
-        for (Field field : ReserveStationTableRow.class.getDeclaredFields()) {
-            String fieldName = field.getName();
-            TableColumn column = new TableColumn(fieldName.substring(1, fieldName.length()));
-            column.setCellFactory(new PropertyValueFactory<ReserveStationTableRow, String>(fieldName));
-            table.getColumns().add(column);
-        }
-        vbox.setSpacing(5);
-        vbox.getChildren().addAll(label, table);
-        return vbox;
-    }
-
     private Node createSecondaryTables() {
         final HBox hbox = new HBox();
         hbox.setSpacing(5);
@@ -79,11 +57,34 @@ public class MainApplication extends Application {
         label.setFont(new Font("Arial", 20));
         final VBox vbox = new VBox();
         TableView table = new TableView();
+        table.setMinSize(Control.USE_PREF_SIZE, Control.USE_PREF_SIZE);
+        table.setMaxSize(Control.USE_PREF_SIZE, Control.USE_PREF_SIZE);
         table.setEditable(false);
-        List<TableColumn> tableColumnList = new ArrayList<TableColumn>();
-        tableColumnList.add(new TableColumn("teste1"));
-        tableColumnList.add(new TableColumn("teste2"));
-        table.getColumns().addAll(tableColumnList);
+        for (Field field : RecentlyUsedMemoryTableRow.class.getDeclaredFields()) {
+            String fieldName = field.getName();
+            TableColumn column = new TableColumn(fieldName.substring(1, fieldName.length()));
+            column.setCellFactory(new PropertyValueFactory<ReserveStationTableRow, String>(fieldName));
+            table.getColumns().add(column);
+        }
+        vbox.setSpacing(5);
+        vbox.getChildren().addAll(label, table);
+        return vbox;
+    }
+
+    private Node createReserveStationTable() {
+        final Label label = new Label("Reserve Stations");
+        label.setFont(new Font("Arial", 20));
+        final VBox vbox = new VBox();
+        TableView table = new TableView();
+        table.setMinSize(Control.USE_PREF_SIZE, Control.USE_PREF_SIZE);
+        table.setMaxSize(Control.USE_PREF_SIZE, Control.USE_PREF_SIZE);
+        table.setEditable(false);
+        for (Field field : ReserveStationTableRow.class.getDeclaredFields()) {
+            String fieldName = field.getName();
+            TableColumn column = new TableColumn(fieldName.substring(1, fieldName.length()));
+            column.setCellFactory(new PropertyValueFactory<ReserveStationTableRow, String>(fieldName));
+            table.getColumns().add(column);
+        }
         vbox.setSpacing(5);
         vbox.getChildren().addAll(label, table);
         return vbox;
