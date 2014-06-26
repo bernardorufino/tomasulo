fce4b054ed608c53d4a2f750104afcda53226dd3
Thais Kagohara <thais.kagohara@gmail.com>
2014-06-26 16:06:56 -0300

    redoing ReserveStationTableRow

--- src/com/abcdel/tomasulo/ui/ReserveStationTableRow.java
+++ src/com/abcdel/tomasulo/ui/ReserveStationTableRow.java
@@ -1,26 +1,66 @@
 package com.abcdel.tomasulo.ui;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ReserveStationTableRow {
 
-  private String mID;
-  private String mType;
-  private String mBusy;
-  private String mState;
-  private String mVj;
-  private String mVk;
-  private String mQj;
-  private String mQk;
-  private String mA;
+    private StringProperty mID;
+    private StringProperty mType;
+    private StringProperty mBusy;
+    private StringProperty mState;
+    private StringProperty mVj;
+    private StringProperty mVk;
+    private StringProperty mQj;
+    private StringProperty mQk;
+    private StringProperty mA;
 
-  public ReserveStationTableRow(String id, String type, String busy, String state, String vj, String vk, String qj, String qk, String a) {
-    mA = a;
-    mType = type;
-    mBusy = busy;
-    mID = id;
-    mState = state;
-    mVj = vj;
-    mVk = vk;
-    mQj = qj;
-    mQk = qk;
-  }
+    public ReserveStationTableRow(String id, String type, String busy, String state, String vj, String vk, String qj, String qk, String a) {
+        mID = new SimpleStringProperty(id);
+        mType = new SimpleStringProperty(type);
+        mBusy = new SimpleStringProperty(busy);
+        mState = new SimpleStringProperty(state);
+        mVj = new SimpleStringProperty(vj);
+        mVk = new SimpleStringProperty(vk);
+        mQj = new SimpleStringProperty(qj);
+        mQk = new SimpleStringProperty(qk);
+        mA = new SimpleStringProperty(a);
+        
+    }
+
+    public StringProperty mIDProperty(){
+        return mID;
+    }
+
+    public StringProperty mTypeProperty(){
+        return mType;
+    }
+
+    public StringProperty mBusyProperty() {
+        return mBusy;
+    }
+
+    public StringProperty mStateProperty() {
+        return mState;
+    }
+
+    public StringProperty mVjProperty() {
+        return mVj;
+    }
+
+    public StringProperty mVkProperty() {
+        return mVk;
+    }
+
+    public StringProperty mQjProperty() {
+        return mQj;
+    }
+
+    public StringProperty mQkProperty() {
+        return mQk;
+    }
+
+    public StringProperty mAProperty() {
+        return mA;
+    }
 }
